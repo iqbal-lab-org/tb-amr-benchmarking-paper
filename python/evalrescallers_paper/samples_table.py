81d43756b1856e4b1d643803c0231c412237e54c
Martin Hunt <martin.g.hunt@gmail.com>
2018-11-26 10:21:36 +0000

    Add country column to samples table

--- python/evalrescallers_paper/samples_table.py
+++ python/evalrescallers_paper/samples_table.py
@@ -7,25 +7,29 @@ def make_samples_tsv(json_dict, outfile):
     json_dict expected to be json object loaded from summary.json
     made by tb-amr-benchmarking pipeline'''
     ten_k_drugs, ten_k_pheno_data_validation, ten_k_pheno_data_test, ten_k_predict_data = ten_k_validation_data.load_all_data()
-    myk_all_drugs, myk_sample_to_res = mykrobe_pub_data.load_all_nature_suppl_files('tb')
+    ten_k_sources = ten_k_validation_data.load_sources_file(ten_k_validation_data.sources_file)
+    ten_k_countries = {x: ten_k_sources[x][1] for x in ten_k_sources}
+    myk_all_drugs, myk_sample_to_res, myk_sample_to_country = mykrobe_pub_data.load_all_nature_suppl_files('tb')
     # don't include Quinolones. It's inferred from the other drugs, and not
     # actually in the original data. The phenos are there as the separate drugs.
     myk_all_drugs.remove('Quinolones')
 
     all_drugs = sorted(list(ten_k_drugs.union(myk_all_drugs)))
     dictionaries = [
-        ('training', myk_sample_to_res),
-        ('validation', ten_k_pheno_data_validation),
-        ('test', ten_k_pheno_data_test),
+        ('training', myk_sample_to_res, myk_sample_to_country),
+        ('validation', ten_k_pheno_data_validation, ten_k_countries),
+        ('test', ten_k_pheno_data_test, ten_k_countries),
     ]
 
     with open(outfile, 'w') as f:
-        print('ENA_id', 'Dataset', *all_drugs, sep='\t', file=f)
+        print('ENA_id', 'Dataset', 'Country', *all_drugs, sep='\t', file=f)
 
-        for dataset_name, dataset_dict in dictionaries:
+        for dataset_name, dataset_dict, country_dict in dictionaries:
             for sample in sorted(dataset_dict):
                 if sample not in json_dict:
                     continue
+
+                country = country_dict[sample]
 
                 phenotypes = []
                 for drug in all_drugs:
@@ -34,5 +38,5 @@ def make_samples_tsv(json_dict, outfile):
                         phenotype = 'U'
                     phenotypes.append(phenotype)
 
-                print(sample, dataset_name, *phenotypes, sep='\t', file=f)
+                print(sample, dataset_name, country, *phenotypes, sep='\t', file=f)
 
