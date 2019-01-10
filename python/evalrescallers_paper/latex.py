f8d64d1680e646b3e451635cec15ac6d37d4c60e
Martin Hunt <martin.g.hunt@gmail.com>
2019-01-10 14:07:15 +0000

    Make regimen summary table

--- python/evalrescallers_paper/latex.py
+++ python/evalrescallers_paper/latex.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 import csv
 
+from evalrescallers_paper import common_data
+
 
 def tool_accuracy_table_on_one_dataset(stats_tsv_file, tool, drugs, dataset, outfile):
     tool_stats = {}
@@ -42,4 +44,37 @@ def tool_accuracy_table_on_one_dataset(stats_tsv_file, tool, drugs, dataset, out
 
         print(r'''\hline''', file=f)
         print(r'''\end{tabular}''', file=f)
+
+
+
+
+def regimen_summary_table(regimen_summary_file, outfile, datasets, tools):
+    counts = {tool: {'right': 0, 'wrong': 0} for tool in tools}
+
+    with open(regimen_summary_file) as f:
+        reader = csv.DictReader(f, delimiter='\t')
+
+        for d in reader:
+            if d['Dataset'] not in datasets or d['Tool'] not in tools or d['Truth_regimen'] == 'NA':
+                continue
+
+            assert d['Tool'] in counts
+            if d['Truth_regimen'] == d['Called_regimen']:
+                key = 'right'
+            else:
+                key = 'wrong'
+
+            counts[d['Tool']][key] += int(d['Count'])
+
+
+    with open(outfile, 'w') as f:
+        print(r'''\begin{tabular}{lrr}''', file=f)
+        print(r'''Tool & Correct regimen & Incorrect regimen \\''', file=f)
+        print(r'''\hline''', file=f)
+        for tool in sorted(list(counts)):
+            print(common_data.tool_names[tool], counts[tool]['right'], counts[tool]['wrong'], end=r''' \\''' + '\n', sep=' & ', file=f)
+
+        print(r'''\hline''', file=f)
+        print(r'''\end{tabular}''', file=f)
+
 
