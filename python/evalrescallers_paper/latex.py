7b55cfbf02afd2dd76ed4803b486e8689000d991
Martin Hunt <martin.g.hunt@gmail.com>
2019-01-11 08:21:41 +0000

    Remove %s from cells

--- python/evalrescallers_paper/latex.py
+++ python/evalrescallers_paper/latex.py
@@ -20,16 +20,18 @@ def tool_accuracy_table_on_one_dataset(stats_tsv_file, tool, drugs, dataset, out
                 ('FP', d['FP']),
                 ('TN', d['TN']),
                 ('FN', d['FN']),
-                ('VME (95\% CI)', f'${d["FNR"]}$\%(${d["FNR_conf_low"]}$-${d["FNR_conf_high"]}$\%)'),
-                ('ME (95\% CI)', f'${d["FPR"]}$\%(${d["FPR_conf_low"]}$-${d["FPR_conf_high"]}$\%)'),
-                ('PPV (95\% CI)', f'${d["PPV"]}$\%(${d["PPV_conf_low"]}$-${d["PPV_conf_high"]}$\%)'),
-                ('NPV (95\% CI)', f'${d["NPV"]}$\%(${d["NPV_conf_low"]}$-${d["NPV_conf_high"]}$\%)'),
+                ('VME (95\% CI)', f'${d["FNR"]}$(${d["FNR_conf_low"]}$-${d["FNR_conf_high"]}$\%)'),
+                ('ME (95\% CI)', f'${d["FPR"]}$(${d["FPR_conf_low"]}$-${d["FPR_conf_high"]}$\%)'),
+                ('PPV (95\% CI)', f'${d["PPV"]}$(${d["PPV_conf_low"]}$-${d["PPV_conf_high"]}$\%)'),
+                ('NPV (95\% CI)', f'${d["NPV"]}$(${d["NPV_conf_low"]}$-${d["NPV_conf_high"]}$\%)'),
             ])
 
     with open(outfile, 'w') as f:
         printed_header = False
 
         for drug in drugs:
+            if drug not in tool_stats:
+                print('Drug not in tool_stats:', drug)
             assert drug in tool_stats
             if not printed_header:
                 print(r'''\begin{tabular}{''', 'c' * len(tool_stats[drug]), '}', sep='', file=f)
