8c77594f3eb3d4c8f2acd5f1fc466fcc0200f582
Martin Hunt <martin.g.hunt@gmail.com>
2019-11-07 10:32:21 +0000

    Add grid to memory and ram boxplots

--- python/evalrescallers_paper/run_time_and_memory.py
+++ python/evalrescallers_paper/run_time_and_memory.py
@@ -37,6 +37,7 @@ def tsv_to_plot(tsv_file, outprefix):
           geom_boxplot() +
           xlab("Tool") +
           ylab("Wall clock time (m)") +
+          theme_linedraw() +
           theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
           scale_y_continuous(trans='log10')
         ggsave(filename="''' + time_pdf + '''", width=6, height=5, scale=0.95)
@@ -46,6 +47,7 @@ def tsv_to_plot(tsv_file, outprefix):
           geom_boxplot() +
           xlab("Tool") +
           ylab("Peak RAM (MB)") +
+          theme_linedraw() +
           theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
           scale_y_continuous(trans='log10')
         ggsave(filename="''' + memory_pdf + '''", width=6, height=5, scale=0.95)
